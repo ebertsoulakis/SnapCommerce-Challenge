a291615221a1ba5ba88d176a276bb9df985e66e2
Emmanuel Bertsoulakis <e.bertsoulakis@gmail.com>
2022-01-23 20:11:19 -0500

    Added line to center column headers. Added more comments

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import string
-from IPython.display import display
-from IPython.display import HTML
+
 #Data that needs to be cleaned
 data = 'Airline Code;DelayTimes;FlightCodes;To_From\nAir Canada (!);[21, 40];20015.0;WAterLoo_NEWYork\n<Air France> (12);[];;Montreal_TORONTO\n(Porter Airways. );[60, 22, 87];20035.0;CALgary_Ottawa\n12. Air France;[78, 66];;Ottawa_VANcouvER\n""".\\.Lufthansa.\\.""";[12, 33];20055.0;london_MONTreal\n'
 
@@ -50,10 +49,11 @@ indexes = []
 #Create dictionary to be used in pandas data frame
 for index in range(len(headers)):
     dataDict[headers[index]] = [x[index] for x in splitsAgain]
-    indexes.append(f"Flight {index}")
+    indexes.append(f"Flight {index+1}")
 
 #create data frame
 table = pd.DataFrame(dataDict, index=indexes)
+pd.set_option('display.colheader_justify', 'center')
 print(table)
 
 
